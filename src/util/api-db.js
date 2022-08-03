0cdb75add197556555a43ca8995b196587c30be9
Sebestyen Orsolya <elfromka@gmail.com>
2022-08-03 21:45:13 +0300

    Small change in api-db.js file

--- src/util/api-db.js
+++ src/util/api-db.js
@@ -3,7 +3,7 @@ const API_KEY = `api_key=${process.env.REACT_APP_API_KEY}`;
 const ApiDb = {
     BASE_URL: "https://api.themoviedb.org/3/",
     IMG_URL: "https://image.tmdb.org/t/p/w500/",
-    API_KEY: API_KEY,
+    API_KEY,
     FALLBACK_IMG_URL: "http://via.placeholder.com/",
 };
 
